384905c54ecc3fea8a67a326cdbb6f1479dd2e9f
Julian Korgol <kontakt@juliankorgol.com>
2023-03-10 16:34:20 +0100

    app ready (on lesson)

--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -3,14 +3,40 @@ import WebSocket from "react-websocket";
 import { Props } from "./index.types";
 
 const Chat: React.FC<Props> = () => {
+    const webSocketRef = React.createRef();
     const [messages, setMessages] = useState<string[]>([])
+    const [newMessage, setNewMessage] = useState<string>("")
 
 
     return <div className="Chat">
-        <WebSocket url={"ws://localhost:8080"} onMessage={(message: string) => {
-            setMessages([...messages, message]);
-        }}
+        <WebSocket
+            url={"ws://localhost:8080"}
+            ref={webSocketRef}
+            onMessage={(message: string) => {
+                setMessages([...messages, message]);
+            }}
         />
+        <div className="messages">
+            {messages.map((message) => (
+                <div key={message} className="message">{message}</div>
+            ))}
+        </div>
+        <div className="form">
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                (webSocketRef.current as any)?.sendMessage(newMessage);
+                setNewMessage("");
+            }}>
+                <input
+                    type="text" 
+                    value={newMessage}
+                    onChange={(event) => {
+                        setNewMessage(event.target.value);
+                    }}
+                />
+                <button type="submit">Wyślij</button>
+            </form>
+        </div>
     </div>;
 };
 
